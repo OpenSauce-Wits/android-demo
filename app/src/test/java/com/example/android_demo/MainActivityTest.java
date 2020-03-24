2f4b0a435891b15b65e18abb3407463a9c6e38e6
thabotr <1831661@students.wits.ac.za>
2020-03-24 12:46:26 +0000

    Exporting CLI project to git for deployment into bitrise.io

---
+++ app/src/test/java/com/example/android_demo/MainActivityTest.java
@@ -0,0 +1,12 @@
+package com.example.android_demo ;
+import static org.junit.Assert.* ;
+import org.junit.Test ;
+
+public class MainActivityTest
+{
+	@Test
+	public void testSomeRandomMethod_ReturnsTrue()
+	{
+		assertTrue( MainActivity.someRandomMethod()) ;
+	}
+}
